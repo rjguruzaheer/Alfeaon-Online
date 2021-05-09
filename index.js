dbd17189e2ff7bcc859c6f3d9f3730132ceadb30
rjguruzaheer <69722710+rjguruzaheer@users.noreply.github.com>
2021-05-09 17:21:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".tg").onclick = function () {
+    location.href = "https://telegram.me/Alfeaongamingworld"
+};
+
+document.querySelector(".app1").onclick = function () {
+    location.href = "test.html"
+};
